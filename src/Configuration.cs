92a3a4fee09dfee2c14fa4333cd576c656655b07
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-07-12 21:12:21 +0200

    Add project files.

---
+++ src/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using Recrovit.RecroGridFramework.Blazor.RgfApexCharts.Components;
+using Recrovit.RecroGridFramework.Client;
+using Recrovit.RecroGridFramework.Client.Blazor;
+using System.Reflection;
+
+namespace Recrovit.RecroGridFramework.Blazor.RgfApexCharts;
+
+public class RgfApexChartsConfiguration
+{
+    public static Task LoadResourcesAsync(IJSRuntime jsRuntime)
+    {
+        var libName = Assembly.GetExecutingAssembly().GetName().Name;
+        //await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.AddStyleSheetLink", $"{RgfClientConfiguration.AppRootPath}_content/{libName}/css/styles.css", false, RgfApexCharts);
+        return Task.CompletedTask;
+    }
+
+    public static Task UnloadResourcesAsync(IJSRuntime jsRuntime)
+    {
+        //await jsRuntime.InvokeVoidAsync("eval", $"document.getElementById('{RgfApexCharts}')?.remove();");
+        return Task.CompletedTask;
+    }
+
+    private static readonly string RgfApexCharts = "rgf-apexcharts";
+}
+
+public static class RgfApexChartsConfigurationExtension
+{
+    public static async Task InitializeRGFBlazorApexChartsAsync(this IServiceProvider serviceProvider, bool loadResources = true)
+    {
+        RgfBlazorConfiguration.RegisterComponent<ChartComponent>(RgfBlazorConfiguration.ComponentType.Chart);
+
+        if (loadResources)
+        {
+            var jsRuntime = serviceProvider.GetRequiredService<IJSRuntime>();
+            await RgfApexChartsConfiguration.LoadResourcesAsync(jsRuntime);
+        }
+        var ver = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+        var logger = serviceProvider.GetRequiredService<ILogger<RgfApexChartsConfiguration>>();
+        logger?.LogInformation($"RecroGrid Framework Blazor ApexCharts v{ver} initialized.");
+    }
+}
